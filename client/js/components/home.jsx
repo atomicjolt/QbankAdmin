3f27253e49158b5fbb5cb450f58f7cece3567171
Justin Ball <justinball@gmail.com>
2016-07-06 15:27:15 -0600

    Change deployment url

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -14,7 +14,7 @@ class Home extends React.Component {
       <div class="main">
         <h2>Providers</h2>
         <div class="providers">
-          <a href="https://eiwc8q1iia.execute-api.us-east-1.amazonaws.com/dev/authentication/signin/google" id="google">Sign In With Google</a>
+          <a href="https://4h8n6sg95j.execute-api.us-east-1.amazonaws.com/dev/authentication/signin/google" id="google">Sign In With Google</a>
         </div>
       </div>
 
