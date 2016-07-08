6a4c409dd3ea286553bdff8f3d1e6a665a27ada6
David Owen <dsowen@fugue88.ws>
2016-07-08 14:21:25 -0600

    Switched to bundled util

--- client/js/components/auth.jsx
+++ client/js/components/auth.jsx
@@ -3,36 +3,26 @@
 import React        from "react";
 import { connect }  from "react-redux";
 
+import { setAuthorization }  from "../actions/auth";
 import history               from "../history";
 import assets                from "../libs/assets";
-import { setAuthorization }  from "../actions/auth";
+import { QueryString }       from "../utils/query_string";
 
 
 @connect((state) => (state.auth), {setAuthorization}, null, {withRef: true})
 class Auth extends React.Component {
 
-  static queryAsMap(query) {
-    let pairs = query.split("&");
-    let params = {};
-
-    for(let p in pairs) {
-      let parts = pairs[p].split("=");
-      let key = parts[0], value = parts[1];
-      params[key] = value;
-    }
-
-    return params;
-  }
-
   componentWillMount() {
-    let query = location.search.slice(1);
-    let params = Auth.queryAsMap(query);
-    this.props.setAuthorization(params.authorization_token, params.refresh_token);
+    let params = QueryString.params();
+    this.props.setAuthorization(params.authorization_token,
+                                params.refresh_token);
   }
 
   componentDidMount() {
     // Now that the user is authenticated, navigate back to the app's root.
-    location.hash = "";
+    let newUrl = /*location.protocol + "//" + location.host +*/ location.pathname;
+    console.log("newUrl", newUrl);
+    history.pushState(null, "title", newUrl);
   }
 
   render() {
