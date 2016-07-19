d11e1c567eca1147039c4dd8933b7f53cca82357
David Owen <dsowen@fugue88.ws>
2016-07-19 16:07:43 -0600

    Removed DUMMY text

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -54,7 +54,7 @@ class Home extends React.Component {
       <li key={bank.id} className={itemClass}>
         <label className="c-checkbox--nested">
           <input type="checkbox" onChange={ (e) => this.checkItem(bank, e.target.checked) }/>
-          <div>{bank.displayName ? bank.displayName.text : "DUMMY"}</div>
+          <div>{bank.displayName.text}</div>
         </label>
         {renderedChildren}
       </li>
