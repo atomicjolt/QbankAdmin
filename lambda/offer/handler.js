e0d2e401b969cd8fcf2ba1a009a5fc10f98865d7
David Owen <dsowen@fugue88.ws>
2016-07-21 13:06:27 -0600

    Function to proxy creation of an offering

---
+++ lambda/offer/handler.js
@@ -0,0 +1,27 @@
+"use strict";
+
+require("ssl-root-cas").inject();
+
+var request = require("superagent");
+
+
+module.exports.handler = function (event, context, callback) {
+  if(context.done === undefined) {
+    console.log(arguments);
+    return;
+  }
+
+  var bank_id = event.bank_id;// .replace(/%/g, "%37");
+  var assessment_id = event.assessment_id;// .replace(/%/g, "%37");
+
+  request.post("https://qbank-clix-dev.mit.edu/api/v1/assessment/banks/" + bank_id + "/assessments/" + assessment_id + "/assessmentsoffered").
+    set("Accept", "application/json").
+    send('{"name":"","description":""}').
+    then((response) => {
+      var offer = JSON.parse(response.text);
+      context.succeed(offer);
+    }, function () {
+      console.log("Failed to create offering", bank_id, assessment_id, arguments);
+      context.fail();
+    });
+};
