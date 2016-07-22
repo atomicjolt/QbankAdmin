25fcedbfaae526d564bdacd38f60be847716fbbb
David Owen <dsowen@fugue88.ws>
2016-07-22 10:03:16 -0600

    Put the tokens into sessionStorage.  Read them back into the initial state if they exist and haven't expired.

--- client/js/reducers/auth.js
+++ client/js/reducers/auth.js
@@ -2,16 +2,40 @@
 
 import {Constants}  from "../actions/auth";
 
-const initialState = {authenticated: false};
+const initialState = (function () {
+  let s = sessionStorage;
+  if(s.expires !== undefined && Date.now() < parseInt(s.expires)) {
+    return {
+      authenticated:  true,
+      auth_token:     s.authorization_token,
+      refresh_token:  s.refresh_token
+    };
+  } else {
+    return {
+      authenticated: false
+    };
+  }
+})();
+
 
 export default (state=initialState, action) => {
   switch(action.type) {
     case Constants.AUTH_SET:
+      sessionStorage.setItem("authorization_token", action.auth_token);
+      sessionStorage.setItem("refresh_token", action.refresh_token);
+
+      // For now, we set our own expiry time.  Later, when QBank requires
+      // signatures and we need to refresh tokens, QBank will tell us when to
+      // expire.
+      sessionStorage.setItem("expires", Date.now() + 1/*hr*/ * 60/*min/hr*/ *
+                             60/*sec/min*/ * 1000/*milli/sec*/);
+
       state = {
         authenticated: true,
         auth_token: action.auth_token,
         refresh_token: action.refresh_token
       };
+      break;
   }
 
   return state;
