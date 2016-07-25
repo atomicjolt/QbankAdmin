6cff4ce789abc68bff2f3c137f168cfc4afc7a2a
Benjamin Call <llacneb@gmail.com>
2016-07-25 14:45:18 -0600

    misspelling of variable being corrected

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -106,9 +106,9 @@ class Home extends React.Component {
     return true;
   }
 
-  offerAssessment(bankdId, assessmentId){
+  offerAssessment(bankId, assessmentId){
     this.setState({fetchingOffered: true});
-    this.props.offerAssessment(bankdId, assessmentId);
+    this.props.offerAssessment(bankId, assessmentId);
   }
 
   renderAssessments(bank, force){
@@ -126,7 +126,7 @@ class Home extends React.Component {
       for(let i in bank.childNodes) {
         let bc = bank.childNodes[i];
         Array.prototype.push.apply(assessmentItems, this.renderAssessments(bc, forceChildren));
-      }
+      }i
     }
     return assessmentItems;
   }
@@ -140,7 +140,7 @@ class Home extends React.Component {
         </svg>
       </div>;
     }
-  }
+  }i
 
   iframe(){
     let assessOffered = this.props.assessment_offered;
