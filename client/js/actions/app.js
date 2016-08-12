c2ffb514880e8cb224b0c0f0cb02b21758e5837c
Nick <nick.benoit14@gmail.com>
2016-08-12 11:57:41 -0600

    remove console.log

--- client/js/actions/app.js
+++ client/js/actions/app.js
@@ -5,7 +5,6 @@ import wrapper from "../constants/wrapper";
 export const Constants = wrapper(["APP_START"], []);
 
 export function startApp(auth_token, qBankHost) {
-  console.log("firing startApp action");
   return {
     type: Constants.APP_START,
     auth_token,
