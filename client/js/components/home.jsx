396a15ccd066d22f5c9333f480b8438468be4f59
David Owen <dsowen@fugue88.ws>
2016-08-16 11:27:23 -0600

    Removed the strange nesting of breadcrumb arrays

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -57,22 +57,28 @@ class Home extends React.Component {
     }, '*');
   }
 
-  renderBreadcrumbs(hierarchy) {
-    return _.map(hierarchy, (bank) => {
-      if(this.state.selectedBanks.has(bank.id)) {
-        return [
-          <div className="c-breadcrumb" key={bank.id}>
-            <span>{bank.displayName.text}</span>
-            <a href="#" onClick={() => { this.onSelectBank(bank, false); }}>
+  gatherBreadcrumbs(breadcrumbs, banks) {
+    banks.forEach((b) => {
+      if(this.state.selectedBanks.has(b.id)) {
+        breadcrumbs.push(
+          <div className="c-breadcrumb" key={b.id}>
+            <span>{b.displayName.text}</span>
+            <a href="#" onClick={() => { this.onSelectBank(b, false); }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
                 <path d="M29.17 16l-5.17 5.17-5.17-5.17-2.83 2.83 5.17 5.17-5.17 5.17 2.83 2.83 5.17-5.17 5.17 5.17 2.83-2.83-5.17-5.17 5.17-5.17-2.83-2.83zm-5.17-12c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 36c-8.82 0-16-7.18-16-16s7.18-16 16-16 16 7.18 16 16-7.18 16-16 16z"/>
               </svg>
             </a>
-          </div>,
-          this.renderBreadcrumbs(bank.childNodes)
-        ];
+          </div>
+        );
       }
+      this.gatherBreadcrumbs(breadcrumbs, b.childNodes);
     });
+  }
+
+  renderBreadcrumbs(hierarchy) {
+    let breadcrumbs = [];
+    this.gatherBreadcrumbs(breadcrumbs, hierarchy);
+    return breadcrumbs;
   }
 
   onExpandBank(bank, value) {
