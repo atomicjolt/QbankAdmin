18555b3f75d1abd31b6250a4dd0d0916a151c154
David Owen <dsowen@fugue88.ws>
2016-08-16 14:53:03 -0600

    Removed dead code

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -20,7 +20,6 @@ class Home extends React.Component {
       openIframe: false,
       isOpen: false,
       expandedBanks: new Set(),
-      itemChecked: {},
       assessments: {}
     };
     Object.freeze(this.state);
