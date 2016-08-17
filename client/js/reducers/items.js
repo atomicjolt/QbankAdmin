bcf7abd6d85c583e43328dc3e37db5747a30b1b2
David Spencer <davidjspencer@ufl.edu>
2016-08-17 14:25:15 -0600

    Add an action to clear items

--- client/js/reducers/items.js
+++ client/js/reducers/items.js
@@ -3,11 +3,14 @@
 import { Constants }   from "../actions/assessments";
 
 
-export default (state = {}, action) => {
+export default (state = [], action) => {
   switch(action.type){
 
     case Constants.ASSESSMENT_ITEMS + Constants.DONE:
       return action.payload;
+
+    case Constants.CLEAR_ASSESSMENT_ITEMS:
+      return [];
 
     default:
       return state;
