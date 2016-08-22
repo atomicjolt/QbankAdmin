010d3f6d615b165295d3423e3529b0989d1743a8
Nick <nick.benoit14@gmail.com>
2016-08-22 14:43:21 -0600

    extract preview sidebar into own component

--- client/js/components/admin_preview.jsx
+++ client/js/components/admin_preview.jsx
@@ -2,58 +2,7 @@ import React from 'react';
 
 import IframeEmbed             from './common/iframe_embed';
 import IframePreview           from './common/iframe_preview';
-
-class PreviewSidebar extends React.Component {
-
-
-  render(){
-    var assessmentName = this.props.assessmentClicked.displayName ? this.props.assessmentClicked.displayName.text : "";
-
-    // If there are no items, render an empty option to clear the select.
-    // Else, map over a range of numbers from 1 to items.length + 1,
-    // and generate an option for each number.
-
-    var nOfMOptions = <option value=""></option>;
-    if(!_.isEmpty(this.props.items)) {
-      nOfMOptions = _.map(_.range(1, this.props.items.length + 1),
-        (index) => {
-          return <option key={index} value={index}>{index}</option>;
-        }
-      );
-    }
-
-    var localeOptions = this.props.locales.map((l) => (
-      <option key={l[0]} value={l[0]}>{l[1]}</option>
-    ));
-
-    return (
-      <div className="c-preview-sidebar">
-        <h2>{assessmentName}</h2>
-        <p>Date Created: <span>02/09/2016</span></p>
-        <p>Type: <span>{this.props.assessmentClicked.type}</span></p>
-        <p>Student must answer
-          <select value={ this.props.nOfM || this.props.items.length }
-                  onChange={(e) => this.props.setNOfM(parseInt(e.target.value))}>
-            { nOfMOptions }
-          </select> of {this.props.items.length}
-        </p>
-
-        <p>Preview the UI in
-          <select onChange={(e) => this.props.onChangeLocale(e)}>
-            {localeOptions}
-          </select>
-        </p>
-        <a
-          style={{"marginTop":"135px"}}
-          href="#"
-          onClick={this.props.displayEmbedCode}
-          className="c-btn  c-btn--previous  c-btn--previous--small">
-          <span>Create Iframe Code</span>
-        </a>
-    </div>
-  );
-  }
-};
+import PreviewSidebar          from './preview_sidebar';
 
 export default class AdminPreview extends React.Component {
 
---
+++ client/js/components/preview_sidebar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default function PreviewSidebar(props) {
+
+  var assessmentName = props.assessmentClicked.displayName ? props.assessmentClicked.displayName.text : "";
+
+  // If there are no items, render an empty option to clear the select.
+  // Else, map over a range of numbers from 1 to items.length + 1,
+  // and generate an option for each number.
+
+  var nOfMOptions = <option value=""></option>;
+  if(!_.isEmpty(props.items)) {
+    nOfMOptions = _.map(_.range(1, props.items.length + 1),
+      (index) => {
+        return <option key={index} value={index}>{index}</option>;
+      }
+    );
+  }
+
+  var localeOptions = props.locales.map((l) => (
+    <option key={l[0]} value={l[0]}>{l[1]}</option>
+  ));
+
+  return (
+    <div className="c-preview-sidebar">
+      <h2>{assessmentName}</h2>
+      <p>Date Created: <span>02/09/2016</span></p>
+      <p>Type: <span>{props.assessmentClicked.type}</span></p>
+      <p>Student must answer
+        <select value={ props.nOfM || props.items.length }
+                onChange={(e) => props.setNOfM(parseInt(e.target.value))}>
+          { nOfMOptions }
+        </select> of {props.items.length}
+      </p>
+
+      <p>Preview the UI in
+        <select onChange={(e) => props.onChangeLocale(e)}>
+          {localeOptions}
+        </select>
+      </p>
+      <a
+        style={{"marginTop":"135px"}}
+        href="#"
+        onClick={props.displayEmbedCode}
+        className="c-btn  c-btn--previous  c-btn--previous--small">
+        <span>Create Iframe Code</span>
+      </a>
+  </div>
+  );
+};
