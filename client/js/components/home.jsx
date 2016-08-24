b39d6ff9207a309298fa43a346c83b2b9509e1d5
Nick <nick.benoit14@gmail.com>
2016-08-24 10:56:33 -0600

    remove old code

--- client/js/components/home.jsx
+++ client/js/components/home.jsx
@@ -69,15 +69,6 @@ export class Home extends React.Component {
     }, '*');
   }
 
-  renderAssessmentList(hierarchy) {
-    let assessmentList = [];
-    _.each(hierarchy, (bank) => {
-      this.gatherAssessments(assessmentList, [], bank);
-    });
-    return assessmentList;
-  }
-
-
   closeAssessmentView() {
     this.props.clearItems();
     this.props.clearAssessmentOffered();
@@ -116,35 +107,6 @@ export class Home extends React.Component {
       this.props.settings.qBankHost,
       nOfM
     );
-  }
-
-  hasNoChildrenSelected(bank) {
-    for(let b of bank.childNodes) {
-      if(this.state.expandedBankPaths.has(b.pathId)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  gatherAssessments(assessmentList, path, bank, force) {
-    path = path.concat([bank.displayName.text]);
-    if(force || this.state.expandedBankPaths.has(bank.pathId)) {
-      if(bank.assessments.length > 0) {
-        assessmentList.push(<h2 className="c-admin-list-location" key={`h_${bank.pathId}`}>{path.join(", ")}</h2>);
-        assessmentList.push(
-          <ul key={`l_${bank.pathId}`}>
-            {bank.assessments.map((a) => (
-               <li key={a.id} className="c-admin-list-item">
-                 <a href="#" onClick={()=>{this.offerAssessment(bank.id, a);}}>{a.displayName.text}</a>
-               </li>
-             ))}
-          </ul>
-        );
-      }
-      let forceChildren = this.hasNoChildrenSelected(bank);
-      bank.childNodes.forEach((b) => this.gatherAssessments(assessmentList, path, b, forceChildren));
-    }
   }
 
   onChangeLocale(event) {
@@ -222,8 +184,8 @@ class AssessmentList extends React.Component {
 
   static propTypes = {
     assessmentClick: React.PropTypes.func,
-    banks: React.PropTypes.object,
-    expandedBankPaths: React.PropTypes.array
+    banks: React.PropTypes.array,
+    expandedBankPaths: React.PropTypes.object
   };
 
   handleAssessmentClick(id, assessment){
